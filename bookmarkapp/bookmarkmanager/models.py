92f8f5c631b4133cda65458b0eaed407e9524c1b
srikanth Marella <srik1040@gmail.com>
2017-08-22 17:48:20 +0530

    Added Models and Admin configuration files

---
+++ bookmarkapp/bookmarkmanager/models.py
@@ -0,0 +1,56 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.timezone import now
+
+
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+# In our example we’ll often show only public bookmarks so we add a second manager to the Bookmark model that will only return the public bookmarks.
+# We’ll assign it to the attribute public. In order to keep a reference to the default manager,
+# we have to explicitly assign it to the objects attribute of the Bookmark class.
+class PublicBookmarkManager(models.Manager):
+    def get_queryset(self):
+        qs = super(PublicBookmarkManager, self).get_queryset()
+        return qs.filter(is_public=True)
+
+@python_2_unicode_compatible
+class Bookmark(models.Model):
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    is_public = models.BooleanField('public', default=True)
+    owner = models.ForeignKey(User, verbose_name='owner', related_name='bookmarks')
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    url = models.URLField()
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    objects = models.Manager()
+    public = PublicBookmarkManager()
+
+    class Meta:
+        verbose_name = 'bookmark'
+        verbose_name_plural = 'bookmarks'
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return '%s (%s) ' % (self.title, self.url)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_created = now()
+        self.date_updated = now()
+        super(Bookmark, self).save(*args, **kwargs)
